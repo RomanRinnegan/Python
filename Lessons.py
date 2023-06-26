37ad39a31e7a6d5df4a03cff471853e711267e94
Roma <mordor.shmordor@gmail.com>
2023-06-26 20:52:08 +0300

    Выполнил ДЗ первого урока

--- Lessons.py
+++ Lessons.py
@@ -1,39 +1,56 @@
-# Задание 1
-from random import randrange
-lst=[randrange(1,10) for i in range(int(input("Введите длинну массива ")))]
-x=int(input('Введите число '))
-a=0
-for i in range(len(lst)):
-    if lst[i]==x:
-        a+=1
-print(lst)
-print("Ваше число встречается", a, "раз.")
+# # # Задание 1
+# for i in range (1, 11):
+#     for j in range (i, i*10, i):
+#         print (j , end='\t')
+#     print()
+#
+# # Задание 2
+# a = int(input("Введите длинну стороны а :", ))
+# b = int(input("Введите длинну стороны b :", ))
+# c = int(input("Введите длинну стороны c :", ))
+# if a + b > c and b + c > a and a + c > b:
+#     print("Треугольник существует!")
+#     if a == b == c:
+#         print("Треугольник равносторонний!")
+#     elif a == b or a == c or b == c:
+#         print("Треугольние равнобедренный!")
+#     else:
+#         print("Треугольник разносторонний!")
+# else:
+#     print("Треугольник не существует!")
+#
+#
+#
+# # Задание 3
+#
+# num = int(input("Введите любое число от 0 до 100тыс.:", ))
+# counter = 0
+# for i in range(1, num+1):
+#     if num % i == 0:
+#         counter +=1
+# if counter <= 2:
+#     print("Число простое")
+# else:
+#     print("Число составное")
+#
+# print("Число делителей:", counter)
 
-# Задание 2
+# Задание 4
+from random import randint
 
-lst=[i for i in range(int(input("Введите длинну массива ")))]
-x=int(input('Введите число '))
-c=0
-for i in lst:
-    if abs(x-lst[i]) < abs(x-c):
-        c=lst[i]
-print(c)
-print(lst)
+num = randint(0, 1000)
+guess = int(input("Угадайте загаданное число:", ))
+counter = 0
+while guess != num and counter < 11:
+    if guess < num :
+        guess = int(input("Мало! Нужно больше!:", ))
+        counter += 1
+    else:
+        guess = int(input("Много! Нужно меньше!:", ))
+        counter += 1
+if guess == num:
+    print(f'Ура! Вы угадали! ваше число: {guess}')
+else:
+    print(f'Cорян. Загаданное число было {num}')
 
-# Задание 3
-
-scrabble = {
-    'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 's': 1, 't': 1, 'r': 1,
-    'd': 2, "g": 2,
-    'b': 3, 'c': 3, 'm': 3, 'p': 3,
-    'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4,
-    'k': 5,
-    'j': 8, 'x': 8,
-    "q":10, 'z': 10
-}
-sum = 0
-lst=input("Введите слово ")
-for i in lst:
-    sum +=scrabble[i]
-print(sum)
 
